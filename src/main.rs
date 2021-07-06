c37667aa0b85cf67a9e37a28f8864fce98e3ef3a
Kimura Takuma <kimura@unirobot.com>
2021-07-06 19:25:10 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use std::{fmt, str::FromStr};
+
+use clap::{App, load_yaml};
+use uuid::{Uuid, v1::{Context, Timestamp}};
+
+enum UUIDFormat {
+    V1,
+    V3,
+    V4,
+    V5,
+}
+
+impl fmt::Display for UUIDFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            UUIDFormat::V1 => write!(f, "v1"),
+            UUIDFormat::V5 => write!(f,"v5"),
+            UUIDFormat::V3 => write!(f,"v3"),
+            UUIDFormat::V4 => write!(f,"v4"),
+        }
+    }
+}
+
+impl FromStr for UUIDFormat {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "v1" => Ok(UUIDFormat::V1),
+            "v5" => Ok(UUIDFormat::V5),
+            "v3" => Ok(UUIDFormat::V3),
+            "v4" => Ok(UUIDFormat::V4),
+            _ => Err(())
+        }
+    }
+}
+
+
+fn main() {
+    let y = load_yaml!("spec/cli.yml");
+    let matches = App::from(y).get_matches();
+
+    
+    if let Some(format) = matches.value_of("format") {
+        let f = UUIDFormat::from_str(format).unwrap();
+
+        let result: Uuid;
+        match f {
+            UUIDFormat::V1 => {
+                let context = Context::new(42);
+                let ts = Timestamp::from_unix(&context, 1497624119, 1234);
+                result = Uuid::new_v1(ts, &[1, 2, 3, 4, 5, 6]).expect("failed to generate UUID");
+            }
+            UUIDFormat::V5 => {
+                let name = "test".as_bytes();
+                let namespace = Uuid::NAMESPACE_DNS;
+                result = Uuid::new_v5(&namespace, name)
+            }
+            UUIDFormat::V3 => {
+                let name = "test".as_bytes();
+                let namespace = Uuid::NAMESPACE_DNS;
+                result = Uuid::new_v3(&namespace, name);
+            }
+            UUIDFormat::V4 => {
+                result = Uuid::new_v4();
+            }
+            _ => unreachable!(),
+        }
+
+        println!("{}", result)
+    } else {
+        println!("<FORMAT> wasn't used...");
+    }
+}
