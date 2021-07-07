2ca825061ae4462254148e7bbef93b0e9c68585d
Kimura Takuma <kimura@unirobot.com>
2021-07-07 12:48:09 +0900

    feat: add flag for copy to clipboard

---
+++ README.md
@@ -0,0 +1,9 @@
+# uuid-gen
+
+## Prerequisites
+
+this CLI using ![rust-clipboard](https://github.com/aweinstock314/rust-clipboard).
+
+> On Linux you need the x11 library, install it with something like:
+> `sudo apt-get install xorg-dev`
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::{fmt, str::FromStr};
 
-use clap::{App, load_yaml};
-use uuid::{Uuid, v1::{Context, Timestamp}};
+use clap::{load_yaml, App};
+use clipboard::{ClipboardContext, ClipboardProvider};
+use uuid::{
+    v1::{Context, Timestamp},
+    Uuid,
+};
 
 enum UUIDFormat {
     V1,
@@ -14,9 +18,9 @@ impl fmt::Display for UUIDFormat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
             UUIDFormat::V1 => write!(f, "v1"),
-            UUIDFormat::V5 => write!(f,"v5"),
-            UUIDFormat::V3 => write!(f,"v3"),
-            UUIDFormat::V4 => write!(f,"v4"),
+            UUIDFormat::V5 => write!(f, "v5"),
+            UUIDFormat::V3 => write!(f, "v3"),
+            UUIDFormat::V4 => write!(f, "v4"),
         }
     }
 }
@@ -30,17 +34,34 @@ impl FromStr for UUIDFormat {
             "v5" => Ok(UUIDFormat::V5),
             "v3" => Ok(UUIDFormat::V3),
             "v4" => Ok(UUIDFormat::V4),
-            _ => Err(())
+            _ => Err(()),
         }
     }
 }
 
+#[derive(Debug)]
+struct Flags {
+    copy: bool,
+}
+
+impl Flags {
+    fn new() -> Self {
+        Self { copy: false }
+    }
+    fn set_copy(&mut self, copy: bool) {
+        self.copy = copy;
+    }
+}
 
 fn main() {
     let y = load_yaml!("spec/cli.yml");
     let matches = App::from(y).get_matches();
 
-    
+    let mut flags = Flags::new();
+    if matches.is_present("copy") {
+        flags.set_copy(true);
+    }
+
     if let Some(format) = matches.value_of("format") {
         let f = UUIDFormat::from_str(format).unwrap();
 
@@ -64,10 +85,18 @@ fn main() {
             UUIDFormat::V4 => {
                 result = Uuid::new_v4();
             }
-            _ => unreachable!(),
         }
 
-        println!("{}", result)
+        println!("{}", result);
+
+        if flags.copy {
+            let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+            let copied = ctx.set_contents(result.to_string());
+            match copied {
+                Ok(_) => println!("copy to clipboard!!"),
+                Err(_) => println!("failed copy to clipboard"),
+            }
+        }
     } else {
         println!("<FORMAT> wasn't used...");
     }
